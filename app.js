692426bc939063d78835061d639aa27c0e04978a
Han0612 <allenwzh@outlook.com>
2023-08-02 10:29:42 -0400

    section 5&6

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express"
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js"
 
 const app = express()
 
+app.use(express.json())
+TuitsController(app)
 HelloController(app)
 UserController(app)
 
-app.listen(4000)
+app.listen(4000)    
--- users/users-controller.js
+++ users/users-controller.js
@@ -5,6 +5,9 @@ let users = people
 const UserController = (app) => {
     app.get('/api/users', FindUsers)
     app.get('/api/users/:uid', FindById)
+    app.post('/api/users', CreateUser)
+    app.delete('/api/users/:uid', DeleteUser)
+    app.put('/api/users/:uid', UpdateUser)
 }
 
 const FindUsers = (req, res) => {
@@ -18,7 +21,7 @@ const FindUsers = (req, res) => {
     res.json(users)
 }
 
-const FindById  = (req, res) => {
+const FindById = (req, res) => {
     const uid = req.params.uid
 
     const userOfId = users.filter(u => u._id === uid)
@@ -26,6 +29,35 @@ const FindById  = (req, res) => {
     res.json(userOfId)
 }
 
+const CreateUser = (req, res) => {
+    const newUser = req.body
+    newUser._id = (new Date()).getTime() + ''
+    
+    users.push(newUser)
+    res.json(newUser)
+}
+
+const DeleteUser = (req, res) => {
+    const userId = req.params['uid']
+
+    users = users.filter(usr => usr._id !== userId)
+
+    res.sendStatus(200)
+}
+
+const UpdateUser = (req, res) => {
+    const userId = req.params['uid']
+    const updates = req.body
+
+    users = users.map((usr) =>
+      usr._id === userId ?
+        {...usr, ...updates} :
+        usr
+    )
+
+    res.sendStatus(200)
+}
+   
 
 
 export default UserController   
