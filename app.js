3ed430f1c32f0ff5343df43a7bfde2d7abb67259
Han0612 <allenwzh@outlook.com>
2023-08-05 09:57:54 -0400

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 import express from "express"
+import cors from 'cors'
+import session from "express-session"
+import "dotenv/config"
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js"
+import AuthController from "./users/auth-controller.js"
 
 const app = express()
 
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+)
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+}
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    }
+}
+app.use(session(sessionOptions))
+  
+app.use(
+    session(sessionOptions)
+)
+
 app.use(express.json())
+
 TuitsController(app)
 HelloController(app)
 UserController(app)
+AuthController(app)
 
-app.listen(4000)    
+app.listen(process.env.PORT || 4000)    
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -7,6 +7,9 @@ const createTuit = (req, res) => {
     newTuit._id = (new Date()).getTime() + ''
     newTuit.likes = 0
     newTuit.liked = false
+    newTuit.dislikes = 0
+    newTuit.replies = 0
+    newTuit.retuits = 0
 
     tuits.push(newTuit)
 
