57edb17db6d617ac935aa3cb662b5aa97334ca58
Han0612 <allenwzh@outlook.com>
2023-08-05 12:27:59 -0400

    update origin

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ import TuitsController from "./controllers/tuits/tuits-controller.js"
 import AuthController from "./users/auth-controller.js"
 
 const app = express()
+app.set("trust proxy", 1)
 
 app.use(
     cors({
       credentials: true,
-      origin: [process.env.FRONTEND_URL, "https://a5--darling-dolphin-e2d1fa.netlify.app"]
+      origin: [process.env.FRONTEND_URL, "https://a5--snazzy-crostata-4443b7.netlify.app/"]
     })
 )
 const sessionOptions = {
